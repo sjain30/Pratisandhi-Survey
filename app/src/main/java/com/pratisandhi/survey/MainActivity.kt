6ec91b796879a3736bc4267f3036790c78303a7c
Sajal Jain <sjain30sept@gmail.com>
2021-02-23 21:47:07 +0530

    Signout added

--- app/src/main/java/com/pratisandhi/survey/MainActivity.kt
+++ app/src/main/java/com/pratisandhi/survey/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.pratisandhi.survey
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.pratisandhi.survey.activities.LoginActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mgoogleSignInClient: GoogleSignInClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.signout, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.logout) {
+
+            // Write your code here to execute after dialog
+            FirebaseAuth.getInstance().signOut()
+            mgoogleSignInClient.signOut()
+            val i = Intent(this@MainActivity, LoginActivity::class.java)
+            i.flags = Intent.FLAG_ACTIVITY_NEW_TASK or
+                    Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(i)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val gso: GoogleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build()
+        mgoogleSignInClient = GoogleSignIn.getClient(this, gso)
     }
 }
